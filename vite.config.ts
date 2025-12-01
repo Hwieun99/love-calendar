de6ebed3e752a42a156bde87ccabdf3341c6a219
Hwieun99 <hwieunlee98@gmail.com>
2025-12-01 22:35:06 +0900

    remove old vite project

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/love-calendar/', // 반드시 GitHub repo 이름과 동일해야 함
-})
-
